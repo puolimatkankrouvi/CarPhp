58e138e58fe1a82f4cfc2ef8e678e2b462006eac
puolimatkankrouvi <cde_1414@yahoo.com>
2017-08-08 17:48:30 +0300

    pqsql

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\DB;
 //Schema
 use Illuminate\Support\Facades\Input;
 
-use Illuminate\Http\Request;
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
